3d83e263e04090c95fdafb7db74f1ac1b9e99e6a
Martin6175903 <martin6175903@yandex.ru>
2025-02-28 00:11:49 +0300

    Added components for Create Account

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,10 +1,23 @@
-import {IonCard, IonCardContent, IonContent, IonHeader, IonInput, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader, IonIcon,
+  IonInput,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
+import { logInOutline, personCircleOutline } from "ionicons/icons";
+import LogoXBox from "../../assets/logo-xbox-svgrepo-com.svg";
+import {FormEvent} from "react";
 
 const Login = () => {
 
-  const handleSubmit = (e: any) => {
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(true)
+    console.log('Login')
   }
 
   return (
@@ -14,12 +27,23 @@ const Login = () => {
           <IonTitle color={'primary'}>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent>
+      <IonContent scrollY={false}>
+        <div className="ion-text-center ion-margin-top">
+          <img src={LogoXBox} alt="Logo FCC" width={150}/>
+        </div>
         <IonCard>
           <IonCardContent>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleLogin}>
               <IonInput fill={"outline"} labelPlacement={"floating"} label={"email"} type={"email"} placeholder={"martin5324@yandex.by"}/>
               <IonInput className={"ion-margin-top"} fill={"outline"} labelPlacement={"floating"} label={"Password"} type={"password"} placeholder={"Skiter2331"}/>
+              <IonButton className={'ion-margin-top'} type={'submit'} expand={'block'}>
+                Login
+                <IonIcon icon={logInOutline} slot={'end'}/>
+              </IonButton>
+              <IonButton routerLink={'/register'} color={'secondary'} type={'button'} expand={'block'} className={'ion-margin-top'}>
+                Create account
+                <IonIcon icon={personCircleOutline} slot={'end'}/>
+              </IonButton>
             </form>
           </IonCardContent>
         </IonCard>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,16 +1,48 @@
-import {IonContent, IonHeader, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import {
+  IonBackButton,
+  IonButton, IonButtons,
+  IonCard,
+  IonCardContent,
+  IonContent,
+  IonHeader, IonIcon,
+  IonInput,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
+import { checkmarkDoneOutline } from "ionicons/icons";
+import {FormEvent} from "react";
 
 const Register = () => {
+
+  const handleRegister = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Create Account')
+  }
+
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Register</IonTitle>
+        <IonToolbar color={'success'}>
+          <IonButtons className={'ion-float-start'}>
+            <IonBackButton color={'light'} defaultHref={'/'}/>
+          </IonButtons>
+          <IonTitle color={'primary'}>Create Account</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className={'ion-padding'}>
-        UI goes here...
-        
+      <IonContent scrollY={false}>
+        <IonCard>
+          <IonCardContent>
+            <form onSubmit={handleRegister}>
+              <IonInput fill={"outline"} labelPlacement={"floating"} label={"email"} type={"email"} placeholder={"martin5324@yandex.by"}/>
+              <IonInput className={"ion-margin-top"} fill={"outline"} labelPlacement={"floating"} label={"Password"} type={"password"} placeholder={"Skiter2331"}/>
+              <IonButton routerLink={'/register'} color={'secondary'} type={'button'} expand={'block'} className={'ion-margin-top'}>
+                Create account
+                <IonIcon icon={checkmarkDoneOutline} slot={'end'}/>
+              </IonButton>
+            </form>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
