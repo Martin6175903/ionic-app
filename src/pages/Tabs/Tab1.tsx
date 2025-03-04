873865d5a5832c2f7bddb2dbf692e97c6e2b3d07
Martin6175903 <martin6175903@yandex.ru>
2025-03-04 23:54:54 +0300

    Added configuration in project

--- src/pages/Tabs/Tab1.tsx
+++ src/pages/Tabs/Tab1.tsx
@@ -1,6 +1,17 @@
-import {IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar} from "@ionic/react";
+import {
+  IonButton,
+  IonButtons, IonCol,
+  IonContent, IonGrid,
+  IonHeader,
+  IonIcon,
+  IonMenuButton,
+  IonPage, IonRow, IonText,
+  IonTitle,
+  IonToolbar
+} from "@ionic/react";
 import {useState} from "react";
 import {Camera, CameraResultType} from "@capacitor/camera";
+import {settings} from "ionicons/icons";
 
 const Tab1 = () => {
 
@@ -30,6 +41,18 @@ const Tab1 = () => {
       <IonContent className={'ion-padding'}>
         <IonButton expand={'full'} onClick={takePicture}>Take Picture</IonButton>
         <img src={image} alt={image}/>
+        <IonButton expand={'full'} className={'ion-margin-horizontal'}>
+          <IonGrid>
+            <IonRow className={'ion-justify-content-between'}>
+              <IonCol className={'ion-text-start'}>
+                <IonText>Настройки</IonText>
+              </IonCol>
+              <IonCol size={'1'}>
+                <IonIcon icon={settings}/>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        </IonButton>
       </IonContent>
     </IonPage>
   );
